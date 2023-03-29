de3e89430948fe38e347c9e6e2e2f1a92a34ae22
THajiyev <75402235+THajiyev@users.noreply.github.com>
2023-03-29 17:55:36 -0500

    Web graphing app

---
+++ graphApp.py
@@ -0,0 +1,90 @@
+import plotly.graph_objs as go
+import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+from analyst import analytics
+import backend
+import json
+
+config = json.load(open('assets/config.json'))
+qual_number = config['qual_number']
+options = [
+    {'label': 'Auton', 'value': 'auton'},
+    {'label': 'Teleop', 'value': 'teleop'},
+    {'label': 'Endgame', 'value': 'endgame'},
+    {'label': 'Total', 'value':'total'}
+]
+
+def get_df(period):
+    df = {}
+    teams = backend.allTeams()
+    for team in teams:
+        team_data = analytics(team).get_point_progression_list(period)
+        df[team] = team_data + [float('nan')] * (qual_number-len(team_data))
+    df = dict(sorted(df.items()))
+    df['x'] = range(1, qual_number+1)
+    return pd.DataFrame(df)
+
+def generate_line_plot(df, columns):
+    layout = go.Layout(
+        width=1500,  
+        height=750
+    )
+    fig = go.Figure(layout=layout)
+    fig.update_xaxes(
+        range=[0, qual_number]
+    )
+    fig.update_layout(
+        xaxis_title='Qual Number',
+        yaxis_title='Points'
+    )
+    for column in columns:
+        fig.add_trace(go.Scatter(x=df['x'], y=df[column], name=column))
+    return fig
+
+def update_graph(fig, df, selected_columns):
+    fig.data = []
+    for column in selected_columns:
+        fig.add_trace(go.Scatter(x=df['x'], y=df[column], name=column))
+    return fig
+
+initial_columns = ['624']
+df = get_df("total")
+fig = generate_line_plot(df, initial_columns)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='team-selector',
+        options=[{'label': column, 'value': column} for column in df.columns[:-1]],
+        value=initial_columns,
+        multi=True
+    ),
+    dcc.Dropdown(
+        id='period-dropdown',
+        options=options,
+        value='total',
+        style={
+            'width': '150px', 
+            'font-size': '20px'
+        }
+    ),
+    dcc.Graph(
+        id='graph',
+        figure=fig
+    ),
+])
+
+@app.callback(
+    dash.dependencies.Output('graph', 'figure'),
+    [dash.dependencies.Input('team-selector', 'value'),
+     dash.dependencies.Input('period-dropdown', 'value')])
+def update_output(selected_columns, value):
+    global df
+    df = get_df(value)
+    return update_graph(fig, df[selected_columns + ['x']], selected_columns)
+
+if __name__ == '__main__':
+    app.run_server()
